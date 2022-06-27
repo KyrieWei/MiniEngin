7df689ec78976db9f52f3295dc2894056f7f7f0b
韦文国 <weiwenguo@ztgame.com>
2022-06-27 19:00:50 +0800

    add reflection::Ptr

--- MiniEngine/Source/Runtime/Core/Meta/Reflection/Reflection.h
+++ MiniEngine/Source/Runtime/Core/Meta/Reflection/Reflection.h
@@ -25,5 +25,37 @@ namespace ME
 		} \
 	};
 
+	template<typename T>
+	class ReflectionPtr
+	{
+		template<typename U>
+		friend class ReflectionPtr;
+
+	public:
+		ReflectionPtr(std::string type_name, T* instance) : m_type_name(type_name), m_instance(instance) {}
+		ReflectionPtr() : m_type_name(), m_instance(nullptr) {}
+
+		ReflectionPtr(const ReflectionPtr& dest) : m_type_name(dest.m_type_name), m_instance(dest.m_instance) {}
+
+		template<typename U>
+		ReflectionPtr<T>& operator=(const ReflectionPtr<U>& dest)
+		{
+			if (this == static_cast<void*>(&dest))
+			{
+				return *this;
+			}
+
+			m_type_name = dest.m_type_name;
+			m_instance = static_cast<T*>(dest.m_instance);
+			return *this;
+		}
+
+
+
+	private:
+		std::string m_type_name{ "" };
+		typedef T m_type;
+		T* m_instance{ nullptr };
+	};
 
 } //namespace MiniEngine
